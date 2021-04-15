369186b7bb6f0c8fffd48fd86032af3236b0a7c0
Yun <yunhengz529@gmail.com>
2021-04-15 11:21:09 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,10 @@
+public class HelloNumbers{
+	public static void main(String[] args){
+		int sum = 0;
+		for(int i=0; i<=9; i++){
+			sum += i;
+			System.out.print(sum + " ");
+		}
+		System.out.println();
+	}
+}
